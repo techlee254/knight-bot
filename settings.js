33e863a37ec460fc9be505221ce9ee4abda5107c
Techlee <techlee047@gmail.com>
2025-12-09 23:38:22 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎bot',
   botName: "Knight Bot",
   botOwner: 'tech lee', // Your name
-  ownerNumber: '254701663762', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '254723617436', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
